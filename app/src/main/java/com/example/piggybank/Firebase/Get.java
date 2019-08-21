2d3816f2e8eb6f17da3000220a2046615b8eaf39
Edward Gervis <edward.gervis@gmail.com>
2019-08-21 17:59:59 -0400

    login working

--- app/src/main/java/com/example/piggybank/Firebase/Get.java
+++ app/src/main/java/com/example/piggybank/Firebase/Get.java
@@ -1,4 +1,44 @@
 package com.example.piggybank.Firebase;
 
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Get {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final static String TAG = Get.class.getName();
+
+    public void getUserById(final String userId,
+                            final OnSuccessListener<User> onSuccessListener,
+                            final OnFailureListener onFailureListener) {
+        db.collection("users").document(userId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    User user;
+                    try {
+                        user = User.SNAPSHOTPARSER.parseSnapshot(documentSnapshot);
+                    } catch (NullPointerException exc) {
+                        user = null;
+                        Log.w(TAG, "User with id " + userId + " was corrupt ", exc);
+                    }
+                    onSuccessListener.onSuccess(user);
+                } else {
+                    Log.e(TAG, "User with id " + userId + " doesn't exit");
+                    onSuccessListener.onSuccess(null);
+                }
+
+
+
+                User user = User.SNAPSHOTPARSER.parseSnapshot(documentSnapshot);
+                onSuccessListener.onSuccess(user);
+            }
+        }).addOnFailureListener(onFailureListener);
+    }
 }
--- app/src/main/java/com/example/piggybank/MainActivity.java
+++ app/src/main/java/com/example/piggybank/MainActivity.java
@@ -1,22 +1,137 @@
 package com.example.piggybank;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.piggybank.Firebase.Get;
+import com.example.piggybank.Firebase.Post;
+import com.example.piggybank.Firebase.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 773;
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            checkSignIn();
+        } else {
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        Log.v(TAG, "Request Code: " + requestCode);
+        Log.v(TAG, "Result Code: " + resultCode);
+
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "Successfully logged in");
+                checkSignIn();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Log.d(TAG, "User did not sign in");
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Log.d(TAG, "No internet connection");
+                    return;
+                }
+
+                Log.e(TAG, "Sign-in error: ", response.getError());
+            }
+
+        }
+    }
+
+    private void checkSignIn() {
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Get get = new Get();
+
+            get.getUserById(auth.getCurrentUser().getUid(), new OnSuccessListener<User>() {
+                @Override
+                public void onSuccess(User user) {
+                    System.out.println(user);
+                    if (user == null) {
+                        Log.v(TAG, "First time user signs in to this app");
+
+                        final String userName = auth.getCurrentUser().getDisplayName();
+
+                        Post post = new Post();
+                        post.createUser(auth.getCurrentUser().getUid(),
+                                userName,
+                                "parent",
+                                new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {System.out.println("test1");
+                                        Intent intent = new Intent(MainActivity.this, UserSelectionActivity.class);
+                                        intent.putExtra("userId",auth.getCurrentUser().getUid());
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }, new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "Error creating the user");
+                                    }
+                                });
+
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, UserSelectionActivity.class);
+                        intent.putExtra("userId", auth.getCurrentUser().getUid());
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG, "Couldn't check if user exists", e);
+                }
+            });
+
+        } else {
+            // This should never happen
+            NullPointerException exc = new NullPointerException("User signed in but null user");
+            Log.e(TAG, "User attempted to sign in but auth wasn't initialized", exc);
+            throw exc;
+        }
     }
 }
