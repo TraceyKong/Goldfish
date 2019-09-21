dd250fc56ecf43323e4dea86cf7d14d365a9e8fe
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2019-09-21 13:16:49 -0400

    child task overview + details

--- app/src/main/java/com/example/piggybank/ChildActivities/TaskDetailsChildActivity.java
+++ app/src/main/java/com/example/piggybank/ChildActivities/TaskDetailsChildActivity.java
@@ -1,4 +1,27 @@
 package com.example.piggybank.ChildActivities;
 
-public class TaskDetailsChildActivity {
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.piggybank.R;
+
+public class TaskDetailsChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details_child);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
 }
--- app/src/main/java/com/example/piggybank/ChildActivities/TasksChildOverview.java
+++ app/src/main/java/com/example/piggybank/ChildActivities/TasksChildOverview.java
@@ -1,15 +1,35 @@
 package com.example.piggybank.ChildActivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.piggybank.Firebase.Get;
+import com.example.piggybank.Firebase.Models.Task;
 import com.example.piggybank.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class TasksChildOverview extends AppCompatActivity {
+    private String childId;
+
+    private RecyclerView recyclerViewTasks;
+    private RecyclerView.Adapter adapterTasks;
+    private RecyclerView.LayoutManager layoutManagerTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +37,115 @@ public class TasksChildOverview extends AppCompatActivity {
         setContentView(R.layout.activity_tasks_child_overview);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Tasks Overview");
 
+        this.childId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        recyclerViewTasks = findViewById(R.id.tasksOverviewChildRV);
+        layoutManagerTasks = new LinearLayoutManager(this);
+        recyclerViewTasks.setLayoutManager(layoutManagerTasks);
+
+        makeRV();
     }
 
+    //returns to previous page
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        makeRV();
+    }
+
+    public void makeRV() {
+        Get get = new Get();
+        get.getTasksByChildId(this.childId, new OnSuccessListener<ArrayList<Task>>() {
+            @Override
+            public void onSuccess(ArrayList<Task> tasks) {
+                TextView text = findViewById(R.id.noTasks2);
+
+                if(tasks.size() == 0)
+                    text.setVisibility(View.VISIBLE);
+                else
+                    text.setVisibility(View.GONE);
+                adapterTasks = new TasksChildOverview.TasksAdapter(tasks);
+                recyclerViewTasks.setAdapter(adapterTasks);
+            }
+        }, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println(e);
+            }
+        });
+    }
+
+    private class TasksAdapter extends RecyclerView.Adapter<TasksChildOverview.TasksAdapter.TaskHolder> {
+
+        private ArrayList<Task> tasks;
+        public TasksAdapter(ArrayList<Task> tasks) {
+            this.tasks = tasks;
+        }
+        @NonNull
+        @Override
+        public TasksChildOverview.TasksAdapter.TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.holder_task, parent, false);
+            return new TasksChildOverview.TasksAdapter.TaskHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TasksChildOverview.TasksAdapter.TaskHolder holder, int position) {
+            //get a task
+            final Task task = tasks.get(position);
+            //set the name of the task to the textview
+            holder.task.setText(task.getName());
+            //add status icon to the right of the textview based on the staus of the task
+            if(task.getStatus().equals("incomplete"))
+                holder.task.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, R.drawable.ic_close,0);
+            if(task.getStatus().equals("completed"))
+                holder.task.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, R.drawable.ic_remove,0);
+            if(task.getStatus().equals("confirmed"))
+                holder.task.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, R.drawable.ic_done,0);
+            //adds onclick to the textview that will open this task's details in a new activity
+            holder.task.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(TasksChildOverview.this, TaskDetailsChildActivity.class);
+                    intent.putExtra("name", getIntent().getExtras().getString("name"));
+                    intent.putExtra("payment", task.getPayment());
+                    intent.putExtra("status", task.getStatus());
+                    intent.putExtra("taskName", task.getName());
+                    intent.putExtra("description", task.getDescription());
+                    intent.putExtra("id", task.getId());
+                    intent.putExtra("childId", childId);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return tasks.size();
+        }
+
+        private class TaskHolder extends RecyclerView.ViewHolder {
+            private TextView task;
+
+            public TaskHolder(@NonNull View itemView) {
+                super(itemView);
+                this.task = itemView.findViewById(R.id.taskName);
+            }
+        }
+    }
 }
